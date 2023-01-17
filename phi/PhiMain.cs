f9b8216b04d28bd415275539059b865384a9e20d
Quaris <quaris314@gmail.com>
2023-01-16 22:37:08 -0600

    phi bugfix: Application.Run won't catch exceptions in release mode

--- phi/PhiMain.cs
+++ phi/PhiMain.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using phi.control;
@@ -9,10 +10,11 @@ namespace phi
 {
    public static class PhiMain
    {
-      
+       
       [STAThread]
       public static void Main(Scene entryScene, DefaultConfig config)
       {
+         Application.ThreadException += new ThreadExceptionEventHandler(HandleThreadException);
          Application.EnableVisualStyles();
          Application.SetCompatibleTextRenderingDefault(false);
          Application.Run(new WindowsForm(entryScene, config));
@@ -21,11 +23,24 @@ namespace phi
       [STAThread]
       public static void Main(Scene entryScene)
       {
+         Application.ThreadException += new ThreadExceptionEventHandler(HandleThreadException);
          Application.EnableVisualStyles();
          Application.SetCompatibleTextRenderingDefault(false);
          Application.Run(new WindowsForm(entryScene, new DefaultConfig()));
       }
 
       public static void Main() { }
+
+      // So APPARENTLY Application.Run will catch any unhandled exceptions itself
+      // BUT ONLY IN RELEASE BUILDS...
+      // Because WHY THE FUCK NOT, apparently
+      // So this method is a workaround to that
+      // See https://stackoverflow.com/questions/6734287/why-is-my-catch-block-only-running-while-debugging-in-visual-studio/6734767#6734767
+      // Fuck whoever made this disabled in debug mode
+      // I spent 3 damn hours pounding this out
+      private static void HandleThreadException(object sender, ThreadExceptionEventArgs e)
+      {
+         throw e.Exception;
+      }
    }
 }
