04a45f01a891c613191a06a5360266762c13d80d
Quaris <quaris314@gmail.com>
2023-01-26 21:47:01 -0600

    phi fixhancement: Made catching crashes more robust

--- phi/PhiMain.cs
+++ phi/PhiMain.cs
@@ -15,6 +15,8 @@ namespace phi
       public static void Main(Scene entryScene, DefaultConfig config)
       {
          Application.ThreadException += new ThreadExceptionEventHandler(HandleThreadException);
+         AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(HandleUnhandledException);
+         Application.SetUnhandledExceptionMode(UnhandledExceptionMode.ThrowException);
          Application.EnableVisualStyles();
          Application.SetCompatibleTextRenderingDefault(false);
          Application.Run(new WindowsForm(entryScene, config));
@@ -24,6 +26,8 @@ namespace phi
       public static void Main(Scene entryScene)
       {
          Application.ThreadException += new ThreadExceptionEventHandler(HandleThreadException);
+         AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(HandleUnhandledException);
+         Application.SetUnhandledExceptionMode(UnhandledExceptionMode.ThrowException);
          Application.EnableVisualStyles();
          Application.SetCompatibleTextRenderingDefault(false);
          Application.Run(new WindowsForm(entryScene, new DefaultConfig()));
@@ -34,13 +38,27 @@ namespace phi
       // So APPARENTLY Application.Run will catch any unhandled exceptions itself
       // BUT ONLY IN RELEASE BUILDS...
       // Because WHY THE FUCK NOT, apparently
-      // So this method is a workaround to that
+      // So thse methods are to get around two separate ways that Application.Run catches exceptions
       // See https://stackoverflow.com/questions/6734287/why-is-my-catch-block-only-running-while-debugging-in-visual-studio/6734767#6734767
       // Fuck whoever made this disabled in debug mode
       // I spent 3 damn hours pounding this out
       private static void HandleThreadException(object sender, ThreadExceptionEventArgs e)
       {
          throw new Exception("Exception from phi", e.Exception);
+      }
+
+      public static void HandleUnhandledException(object sender, UnhandledExceptionEventArgs e)
+      {
+         Exception ex;
+         try
+         {
+            ex = (Exception)e.ExceptionObject;
+         }
+         catch (Exception)
+         {
+            ex = new Exception("A really bad crash happened, this is all the info:\n" + e.ExceptionObject.ToString());
+         }
+         throw new Exception("Exception from phi", ex);
       }
    }
 }
