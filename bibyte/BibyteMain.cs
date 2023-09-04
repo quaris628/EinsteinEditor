1f00a124d2ee80bf8c008a10fc01f92b907d4c6c
Quaris <quaris314@gmail.com>
2023-09-03 22:30:50 -0500

    Created basis for bibyte, a programming interface for creating bibite brains

---
+++ bibyte/BibyteMain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Einstein.model.json;
+
+namespace Bibyte
+{
+   public class BibyteMain
+   {
+      public static void Main(string[] args)
+      {
+         Console.WriteLine("Building brain...");
+         JsonBrain brain = BrainCreator.GenerateBrain();
+         Console.WriteLine("Saving brain...");
+         SaveBrain(brain, BrainCreator.BB8_FILE_TO_SAVE_TO);
+      }
+
+      public static void SaveBrain(JsonBrain brain, string filepath)
+      {
+         string brainJson = brain.GetSave();
+         if (!File.Exists(filepath))
+         {
+            Console.WriteLine("Error, file does not exist: '" + filepath + "'");
+            return;
+         }
+         string bibiteJson = File.ReadAllText(filepath);
+         int startIndex = bibiteJson.IndexOf("\"brain\":") + "\"brain\":".Length;
+         int endIndex = bibiteJson.IndexOf("\"immuneSystem\":");
+         if (startIndex < 0 || endIndex < 0)
+         {
+            Console.WriteLine("Error, file format is invalid. Be sure you're saving to a .bb8 file. Template bibites aren't supported.");
+            return;
+         }
+         bibiteJson = bibiteJson.Substring(0, startIndex) + " " + brainJson + bibiteJson.Substring(endIndex);
+         File.WriteAllText(filepath, bibiteJson);
+         Console.WriteLine("Brain saved");
+      }
+
+   }
+}
---
+++ bibyte/BrainCreator.cs
@@ -0,0 +1,33 @@
+﻿using Einstein;
+using Einstein.model;
+using Einstein.model.json;
+
+namespace Bibyte
+{
+   public static partial class BrainCreator
+   {
+      public static string BB8_FILE_TO_SAVE_TO =
+         "C:/<YOUR BIBITES DIRECTORY HERE>/Bibites/<BIBITE NAME>.bb8";
+      public static JsonBrain GenerateBrain()
+      {
+         JsonSynapse moveForward = new JsonSynapse(Inputs.CONSTANT, Outputs.ACCELERATE, 1);
+         JsonSynapse turnToPlant = new JsonSynapse(Inputs.PLANT_ANGLE, Outputs.ROTATE, 1);
+
+         JsonNeuron hiddenGauss = createNeuron(NeuronType.Gaussian);
+         JsonNeuron hiddenSine = createNeuron(NeuronType.Sine, "Sine");
+
+         JsonSynapse[] otherSynapses = {
+            new JsonSynapse(Inputs.PLANT_CLOSENESS, hiddenGauss, -2),
+            new JsonSynapse(hiddenGauss, hiddenSine, 8.675309f),
+            new JsonSynapse(hiddenGauss, Outputs.PHERE_OUT_3, 100),
+         };
+
+         // for larger brains you'll probably want to organize synapses in sub-collections
+         return buildBrainFrom(otherSynapses,
+            new JsonSynapse[] { moveForward, turnToPlant });
+
+         // but this method works better for smaller brains
+         //return buildBrainFrom(moveForward, turnToPlant);
+      }
+   }
+}
