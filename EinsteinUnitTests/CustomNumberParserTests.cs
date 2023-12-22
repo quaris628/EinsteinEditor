ee025590ce25cae8220697dd797aa7a461363576
Quaris <quaris314@gmail.com>
2023-12-22 01:09:43 -0600

    Add unit tests for float functions in CustomNumberParser

--- EinsteinUnitTests/CustomNumberParserTests.cs
+++ EinsteinUnitTests/CustomNumberParserTests.cs
@@ -8,7 +8,9 @@ namespace EinsteinUnitTests
     [TestClass]
     public class CustomNumberParserTests
     {
-        private static readonly (string str, int i)[] NORMAL_TESTS = new (string str, int i)[]
+        #region Integers
+
+        private static readonly (string, int)[] NORMAL_INT_TESTS = new (string, int)[]
         {
             ("0", 0),
             ("1", 1),
@@ -31,7 +33,7 @@ namespace EinsteinUnitTests
             ("-2147483648", -2147483648),
         };
 
-        private static readonly string[] INVALID_STRING_NOT_A_NUMBER_TESTS = new string[]
+        private static readonly string[] INVALID_STRING_NOT_AN_INT_TESTS = new string[]
         {
             "",
             " ",
@@ -46,7 +48,7 @@ namespace EinsteinUnitTests
             "-35-",
         };
 
-        private static readonly string[] INVALID_ARITHMETIC_OVERFLOW_TESTS = new string[]
+        private static readonly string[] INVALID_ARITHMETIC_OVERFLOW_INT_TESTS = new string[]
         {
             "2147483648",
             "-2147483649",
@@ -55,18 +57,18 @@ namespace EinsteinUnitTests
         [TestMethod]
         public void IntToStringNormal()
         {
-            foreach ((string str, int i) test in NORMAL_TESTS)
+            foreach ((string str, int num) test in NORMAL_INT_TESTS)
             {
-                Assert.AreEqual(test.str, CustomNumberParser.IntToString(test.i));
+                Assert.AreEqual(test.str, CustomNumberParser.IntToString(test.num));
             }
         }
 
         [TestMethod]
         public void StringToIntNormal()
         {
-            foreach ((string str, int i) test in NORMAL_TESTS)
+            foreach ((string str, int num) test in NORMAL_INT_TESTS)
             {
-                Assert.AreEqual(test.i, CustomNumberParser.StringToInt(test.str));
+                Assert.AreEqual(test.num, CustomNumberParser.StringToInt(test.str));
             }
         }
 
@@ -87,7 +89,7 @@ namespace EinsteinUnitTests
         [TestMethod]
         public void StringToIntInvalid_NotANumber_ArgumentException()
         {
-            foreach (string test in INVALID_STRING_NOT_A_NUMBER_TESTS)
+            foreach (string test in INVALID_STRING_NOT_AN_INT_TESTS)
             {
                 bool exceptionThrown = false;
                 try
@@ -105,7 +107,7 @@ namespace EinsteinUnitTests
         [TestMethod]
         public void StringToIntInvalid_ArithmeticOverflow_ArithmeticException()
         {
-            foreach (string test in INVALID_ARITHMETIC_OVERFLOW_TESTS)
+            foreach (string test in INVALID_ARITHMETIC_OVERFLOW_INT_TESTS)
             {
                 bool exceptionThrown = false;
                 try
@@ -120,5 +122,129 @@ namespace EinsteinUnitTests
             }
         }
 
+        #endregion Integers
+
+        #region Floats
+
+        private static readonly (string str, float num)[] NORMAL_FLOAT_TESTS = new (string str, float num)[]
+        {
+            ("0", 0),
+            ("1", 1),
+            ("-1", -1),
+            ("9", 9),
+            ("10", 10),
+            ("11", 11),
+            ("99", 99),
+            ("100", 100),
+            ("101", 101),
+            ("-99", -99),
+            ("-100", -100),
+            ("-101", -101),
+            ("1378314", 1378314),
+            ("-4968451", -4968451),
+            ("2147483600", 2147483600),
+            ("-2147483600", -2147483600),
+            ("2147.483600", 2147.483600f),
+            ("-2147.483600", -2147.483600f),
+            ("340282356779000000000000000000000000000", 340282356779000000000000000000000000000f),
+            ("-340282356779000000000000000000000000000", -340282356779000000000000000000000000000f),
+            //("340282347000000000000000000000000000000", 340282356779733660000000000000000000000f),
+            ("0.3", 0.3f),
+            ("-3.1", -3.1f),
+        };
+
+        private static readonly string[] INVALID_STRING_NOT_A_FLOAT_TESTS = new string[]
+        {
+            "",
+            " ",
+            "-",
+            "?",
+            "A",
+            "4A",
+            "A4",
+            "-35-",
+        };
+
+        private static readonly string[] INVALID_ARITHMETIC_OVERFLOW_FLOAT_TESTS = new string[]
+        {
+            "340282356780000000000000000000000000000",
+            "-340282356780000000000000000000000000000",
+        };
+
+        [TestMethod]
+        public void FloatToStringNormal()
+        {
+            foreach ((string str, float num) test in NORMAL_FLOAT_TESTS)
+            {
+                Assert.AreEqual(test.str, CustomNumberParser.FloatToString(test.num));
+            }
+        }
+
+        [TestMethod]
+        public void StringToFloatNormal()
+        {
+            foreach ((string str, int num) test in NORMAL_INT_TESTS)
+            {
+                Assert.AreEqual(test.num, CustomNumberParser.StringToFloat(test.str));
+            }
+        }
+
+        [TestMethod]
+        public void StringToFloatNormal_LeadingAndTrailingZeros()
+        {
+            Assert.AreEqual(0, CustomNumberParser.StringToFloat("000"), float.Epsilon);
+            Assert.AreEqual(0, CustomNumberParser.StringToFloat("00.00"), float.Epsilon);
+            Assert.AreEqual(3, CustomNumberParser.StringToFloat("003"), float.Epsilon);
+            Assert.AreEqual(3.6f, CustomNumberParser.StringToFloat("003.6"), float.Epsilon);
+            Assert.AreEqual(3.6f, CustomNumberParser.StringToFloat("3.600"), float.Epsilon);
+            Assert.AreEqual(3.6f, CustomNumberParser.StringToFloat("003.600"), float.Epsilon);
+            Assert.AreEqual(3, CustomNumberParser.StringToFloat("00000000000000000000000000000000000000000003"), float.Epsilon);
+            Assert.AreEqual(3.6f, CustomNumberParser.StringToFloat("00000000000000000000000000000000000000000003.6"), float.Epsilon);
+            Assert.AreEqual(3.6f, CustomNumberParser.StringToFloat("00000000000000000000000000000000000000000003.60000000000000000000000000000000000000000000"), float.Epsilon);
+        }
+
+        [TestMethod]
+        public void StringToFloatNormal_NegativeZero()
+        {
+            Assert.AreEqual(0, CustomNumberParser.StringToFloat("-0"), float.Epsilon);
+        }
+
+        [TestMethod]
+        public void StringToFloatInvalid_NotANumber_ArgumentException()
+        {
+            foreach (string test in INVALID_STRING_NOT_A_FLOAT_TESTS)
+            {
+                bool exceptionThrown = false;
+                try
+                {
+                    CustomNumberParser.StringToFloat(test);
+                }
+                catch (ArgumentException)
+                {
+                    exceptionThrown = true;
+                }
+                Assert.IsTrue(exceptionThrown);
+            }
+        }
+
+        [TestMethod]
+        public void StringToFloatInvalid_ArithmeticOverflow_ArithmeticException()
+        {
+            foreach (string test in INVALID_ARITHMETIC_OVERFLOW_FLOAT_TESTS)
+            {
+                bool exceptionThrown = false;
+                try
+                {
+                    CustomNumberParser.StringToFloat(test);
+                }
+                catch (ArithmeticException)
+                {
+                    exceptionThrown = true;
+                }
+                Assert.IsTrue(exceptionThrown);
+            }
+        }
+
+        #endregion Floats
     }
 }
