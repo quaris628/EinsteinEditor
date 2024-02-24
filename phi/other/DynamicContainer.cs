fbaf51d76318164161986692e7a21d0b6b95e9a7
Quaris <quaris314@gmail.com>
2024-02-23 21:49:06 -0600

    phi enhancement: Add OnChangeFlagged subscribable event to DynamicContainer

--- phi/other/DynamicContainer.cs
+++ phi/other/DynamicContainer.cs
@@ -6,19 +6,34 @@ using System.Threading.Tasks;
 
 namespace phi.other
 {
-   /**
+    /**
     * A thing that holds objects that can change.
     * Each object can message this container with FlagChange() when
     *    the object itself has changed.
     * @Author Nathan Swartz
     */
-   public class DynamicContainer
-   {
-      private bool changed;
+    public class DynamicContainer
+    {
+        private bool changed;
+        public delegate void OnChange(DynamicContainer container);
+        public event OnChange OnChangeFlagged;
 
-      public DynamicContainer() { changed = true; }
-      public void FlagChange() { changed = true; }
-      protected void UnflagChanges() { changed = false; }
-      protected bool HasChanged() { return changed; }
-   }
+        public DynamicContainer()
+        {
+            changed = true;
+        }
+        public void FlagChange()
+        {
+            changed = true;
+            OnChangeFlagged?.Invoke(this);
+        }
+        protected void UnflagChanges()
+        {
+            changed = false;
+        }
+        protected bool HasChanged()
+        {
+            return changed;
+        }
+    }
 }
